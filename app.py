2ccebc46a1678b5789040c61f89e558cdeb700ba
Carlos Aldana <carlosaldanaoficial@gmail.com>
2025-08-19 07:53:50 -0600

    Se implementaron columnas para elegir mejor entre botones o checkbox

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ Requisitos: pip install streamlit pandas plotly
 """
 import streamlit as st, pandas as pd, plotly.express as px
 
+# page config
+st.set_page_config(page_title="Análisis Spotify 2024", layout="wide")
+
 # Cargar dataframe
 
 df = pd.read_csv(r"./data/songs_2024.csv", encoding="ISO-8859-1")
@@ -65,136 +68,143 @@ order_rev = order[::-1]
 st.subheader("Datos de Spotify")
 st.dataframe(spotify_df.head())
 
-# Botón para mostrar el gráfico de barras
-bar_button = st.button("Mostrar Top 10 Canciones", key="top_10_songs")
+# Crear columnas para mejor distribución de los componentes
+col1, col2 = st.columns(2)
 
-if bar_button:
-    st.write("Creación del Gráfico de Barras con las 10 canciones más escuchadas")
-    fig = px.bar(
-        top_10_songs,
-        y="track",
-        x="spotify_streams",
-        orientation="h",
-        color="artist",
-        category_orders={"track": order_rev},
-        title="Top 10 Canciones Más Escuchadas en 2024",
-        labels={
-            "track": "Canción",
-            "spotify_streams": "Streams",
-            "artist": "Artista",
-        },
-        color_discrete_sequence=px.colors.qualitative.Pastel,
+with col1:
+    # Botón para mostrar el gráfico de barras
+    bar_button = st.button("Mostrar Top 10 Canciones", key="top_10_songs")
+
+    if bar_button:
+        st.write("Creación del Gráfico de Barras con las 10 canciones más escuchadas")
+        fig = px.bar(
+            top_10_songs,
+            y="track",
+            x="spotify_streams",
+            orientation="h",
+            color="artist",
+            category_orders={"track": order_rev},
+            title="Top 10 Canciones Más Escuchadas en 2024",
+            labels={
+                "track": "Canción",
+                "spotify_streams": "Streams",
+                "artist": "Artista",
+            },
+            color_discrete_sequence=px.colors.qualitative.Pastel,
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+        # Botón para mostrar el histograma de Streams según Popularidad
+
+    hist_button = st.button(
+        "Mostrar Histograma de Streams por Popularidad", key="histogram"
     )
-    st.plotly_chart(fig, use_container_width=True)
 
-# Botón para mostrar el histograma de Streams según Popularidad
+    if hist_button:
+        st.write("Creación del Histograma de Streams por Popularidad")
+        fig = px.histogram(
+            spotify_df,
+            x="spotify_streams",
+            nbins=50,
+            color="popularity_bin",
+            category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
+            title="Streams según Popularidad (1–10)",
+            color_discrete_sequence=px.colors.sequential.Viridis,
+        )
+        fig.update_layout(
+            xaxis_title="Número de Streams",
+            yaxis_title="Número de Canciones",
+            legend_title="Popularidad",
+        )
+        fig.update_traces(marker=dict(line=dict(width=1, color="black")))
 
-hist_button = st.button(
-    "Mostrar Histograma de Streams por Popularidad", key="histogram"
-)
+        st.plotly_chart(fig, use_container_width=True)
 
-if hist_button:
-    st.write("Creación del Histograma de Streams por Popularidad")
-    fig = px.histogram(
-        spotify_df,
-        x="spotify_streams",
-        nbins=50,
-        color="popularity_bin",
-        category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
-        title="Streams según Popularidad (1–10)",
-        color_discrete_sequence=px.colors.sequential.Viridis,
+    scatter_button = st.button("Mostrar Gráfico de Dispersión", key="scatter_plot")
+
+    if scatter_button:
+        st.write("Creación del Gráfico de Dispersión")
+        fig = px.scatter(
+            spotify_df,
+            x="release_year",
+            y="spotify_streams",
+            color="popularity_bin",  # para darle un gradiente de color
+            size="popularity_bin",  # tamaño según
+            category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
+            title="Año de Lanzamiento vs Streams(log) en Spotify",
+            opacity=0.4,
+            color_discrete_sequence=px.colors.qualitative.Bold,
+        )
+        fig.update_layout(xaxis_title="Año de Lanzamiento", legend_title="Popularidad")
+
+        fig.update_yaxes(type="log", title="Número de Streams (log)")
+        st.plotly_chart(fig, use_container_width=True)
+
+
+with col2:
+    build_bar = st.checkbox("Mostrar Gráfico de Barras", key="bar_plot_check")
+
+    if build_bar:
+        st.write("Creación del Gráfico de Barras con las 10 canciones más escuchadas")
+        fig = px.bar(
+            top_10_songs,
+            y="track",
+            x="spotify_streams",
+            orientation="h",
+            color="artist",
+            category_orders={"track": order_rev},
+            title="Top 10 Canciones Más Escuchadas en 2024",
+            labels={
+                "track": "Canción",
+                "spotify_streams": "Streams",
+                "artist": "Artista",
+            },
+            color_discrete_sequence=px.colors.qualitative.Pastel,
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+    build_hist = st.checkbox(
+        "Mostrar Histograma de Streams por Popularidad", key="histogram_check"
     )
-    fig.update_layout(
-        xaxis_title="Número de Streams",
-        yaxis_title="Número de Canciones",
-        legend_title="Popularidad",
+
+    if build_hist:
+        st.write("Creación del Histograma de Streams por Popularidad")
+        fig = px.histogram(
+            spotify_df,
+            x="spotify_streams",
+            nbins=50,
+            color="popularity_bin",
+            category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
+            title="Streams según Popularidad (1–10)",
+            color_discrete_sequence=px.colors.sequential.Viridis,
+        )
+        fig.update_layout(
+            xaxis_title="Número de Streams",
+            yaxis_title="Número de Canciones",
+            legend_title="Popularidad",
+        )
+        fig.update_traces(marker=dict(line=dict(width=1, color="black")))
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    build_scatter = st.checkbox(
+        "Mostrar Gráfico de Dispersión", key="scatter_plot_check"
     )
-    fig.update_traces(marker=dict(line=dict(width=1, color="black")))
 
-    st.plotly_chart(fig, use_container_width=True)
+    if build_scatter:
+        st.write("Creación del Gráfico de Dispersión")
+        fig = px.scatter(
+            spotify_df,
+            x="release_year",
+            y="spotify_streams",
+            color="popularity_bin",  # para darle un gradiente de color
+            size="popularity_bin",  # tamaño según
+            category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
+            title="Año de Lanzamiento vs Streams(log) en Spotify",
+            opacity=0.4,
+            color_discrete_sequence=px.colors.qualitative.Bold,
+        )
+        fig.update_layout(xaxis_title="Año de Lanzamiento", legend_title="Popularidad")
 
-scatter_button = st.button("Mostrar Gráfico de Dispersión", key="scatter_plot")
-
-if scatter_button:
-    st.write("Creación del Gráfico de Dispersión")
-    fig = px.scatter(
-        spotify_df,
-        x="release_year",
-        y="spotify_streams",
-        color="popularity_bin",  # para darle un gradiente de color
-        size="popularity_bin",  # tamaño según
-        category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
-        title="Año de Lanzamiento vs Streams(log) en Spotify",
-        opacity=0.4,
-        color_discrete_sequence=px.colors.qualitative.Bold,
-    )
-    fig.update_layout(xaxis_title="Año de Lanzamiento", legend_title="Popularidad")
-
-    fig.update_yaxes(type="log", title="Número de Streams (log)")
-    st.plotly_chart(fig, use_container_width=True)
-
-
-build_bar = st.checkbox("Mostrar Gráfico de Barras", key="bar_plot_check")
-
-if build_bar:
-    st.write("Creación del Gráfico de Barras con las 10 canciones más escuchadas")
-    fig = px.bar(
-        top_10_songs,
-        y="track",
-        x="spotify_streams",
-        orientation="h",
-        color="artist",
-        category_orders={"track": order_rev},
-        title="Top 10 Canciones Más Escuchadas en 2024",
-        labels={
-            "track": "Canción",
-            "spotify_streams": "Streams",
-            "artist": "Artista",
-        },
-        color_discrete_sequence=px.colors.qualitative.Pastel,
-    )
-    st.plotly_chart(fig, use_container_width=True)
-
-build_hist = st.checkbox(
-    "Mostrar Histograma de Streams por Popularidad", key="histogram_check"
-)
-
-if build_hist:
-    st.write("Creación del Histograma de Streams por Popularidad")
-    fig = px.histogram(
-        spotify_df,
-        x="spotify_streams",
-        nbins=50,
-        color="popularity_bin",
-        category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
-        title="Streams según Popularidad (1–10)",
-        color_discrete_sequence=px.colors.sequential.Viridis,
-    )
-    fig.update_layout(
-        xaxis_title="Número de Streams",
-        yaxis_title="Número de Canciones",
-        legend_title="Popularidad",
-    )
-    fig.update_traces(marker=dict(line=dict(width=1, color="black")))
-
-    st.plotly_chart(fig, use_container_width=True)
-
-build_scatter = st.checkbox("Mostrar Gráfico de Dispersión", key="scatter_plot_check")
-
-if build_scatter:
-    st.write("Creación del Gráfico de Dispersión")
-    fig = px.scatter(
-        spotify_df,
-        x="release_year",
-        y="spotify_streams",
-        color="popularity_bin",  # para darle un gradiente de color
-        size="popularity_bin",  # tamaño según
-        category_orders={"popularity_bin": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
-        title="Año de Lanzamiento vs Streams(log) en Spotify",
-        opacity=0.4,
-        color_discrete_sequence=px.colors.qualitative.Bold,
-    )
-    fig.update_layout(xaxis_title="Año de Lanzamiento", legend_title="Popularidad")
-
-    fig.update_yaxes(type="log", title="Número de Streams (log)")
-    st.plotly_chart(fig, use_container_width=True)
+        fig.update_yaxes(type="log", title="Número de Streams (log)")
+        st.plotly_chart(fig, use_container_width=True)
